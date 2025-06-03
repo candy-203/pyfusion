11448b32d504a49389e0bebfea542488aebb9916
candy203 <jan@kandyba.de>
2025-06-03 14:59:05 +0200

    update

--- src/pyfusion/algs/eed.py
+++ src/pyfusion/algs/eed.py
@@ -1,12 +1,8 @@
-from typing import Callable
-
-import numpy as np
-
 from pyfusion.structs import math
-from pyfusion.utils import operations, projection
+from pyfusion.utils import operations
 
 
-def step(image: math.Image, tensor_field: math.TensorField, step_size: float, sigma: float) -> math.Image:
+def step(image: math.Image, tensor_field: math.TensorField, step_size: float) -> math.Image:
     """
     Perform a single step of diffusion using a tensor field.
 
@@ -19,9 +15,8 @@ def step(image: math.Image, tensor_field: math.TensorField, step_size: float, si
     Returns:
         structs.Image: The resulting image after diffusion.
     """
-    smoothed_image = operations.smooth(image, sigma)
     # Calculate the gradient of the smoothed image
-    grad = operations.nabla(smoothed_image)
+    grad = operations.nabla(image)
 
     # Apply the diffusion tensor to the gradient
     grad_tensor = operations.apply_tensor_field(tensor_field, grad)
@@ -31,35 +26,3 @@ def step(image: math.Image, tensor_field: math.TensorField, step_size: float, si
     div.data *= step_size
     div.data += image.data
     return math.Image(data=div.data)
-
-
-def apply_heat_conduction(
-    tensor_field: math.EigenDecomp, heat_conduction: Callable[[math.FloatArr], math.FloatArr]
-) -> math.TensorField:
-    """
-    Apply heat conduction to the eigenvalues of a tensor field.
-
-    Args:
-        tensor_field (structs.EigenDecomp): The tensor field.
-        heat_conduction (Callable[[structs.FloatArr], structs.FloatArr]): The heat conduction function to apply.
-
-    Returns:
-        structs.TensorField: The resulting tensor field after applying heat conduction, as follows:
-            \\lambda_1 = \\lambda_1 \\cdot \\text{heat_conduction}(\\mu_1)
-            \\lambda_2 = 1
-        The eigenvalues are modified according to the heat conduction function, while the eigenvectors remain unchanged.
-    """
-    ones = np.ones(tensor_field.eigen_2.eigval.shape)
-    eigval_1 = heat_conduction(tensor_field.eigen_1.eigval)
-
-    eigen_1 = math.Eigen(
-        eigvec=tensor_field.eigen_1.eigvec,
-        eigval=eigval_1,
-    )
-    eigen_2 = math.Eigen(
-        eigvec=tensor_field.eigen_2.eigvec,
-        eigval=ones,
-    )
-    eigen_decomp = math.EigenDecomp(eigen_1=eigen_1, eigen_2=eigen_2)
-
-    return projection.calculate_eigen(eigen_decomp)
